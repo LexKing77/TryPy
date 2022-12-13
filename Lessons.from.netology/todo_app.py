51c7d9beddaf1c631b963ffe263019de6ef0f807
LexKing77 <lexking.aid@gmail.com>
2022-12-13 19:54:36 +0300

    command "show" changes (todo_app)

--- Lessons.from.netology/todo_app.py
+++ Lessons.from.netology/todo_app.py
@@ -5,7 +5,6 @@ add - добавить задачу в список (название задач
 show - напечатать все добавленные задачи.
 exit - прекращение работы бота."""
 
-tasks = []
 today = []
 tomorrow = []
 other_days = []
@@ -17,17 +16,12 @@ while run:
     if command == "help":
         print(HELP)
     elif command == "show":
-        list = input('Which list to show?: \n1. Tasks \n2. Today \n3. Tomorrow \n4. Other Days \n')
-        if list == "tasks" or "Tasks" or "1":
-            print(tasks)
-        elif list == "today" or "Today" or "2":
-            print(today)
-        elif list == "tomorrow" or "Tomorrow" or "3":
-            print(tomorrow)
-        elif list == "other days" or "Other days" or "4":
-            print(other_days)
-        else:
-            print('Please, enter the one of available lists')
+        print('Today')
+        print(today)
+        print('Tomorrow')
+        print(tomorrow)
+        print('Others')
+        print(other_days)
     elif command == "add":
         task = input('Please, enter the task name: ')
         date = input('What date is the task due?: ')
@@ -37,8 +31,7 @@ while run:
             tomorrow.append(task)
         else:
             other_days.append(task)
-        # tasks.append(task)
-        print("Task is added")
+        print(f'Task {task} is added on the {date} date')
     elif command == "exit":
         run = False
         print("Bot is disabled. \nThanks for using me! Goodbye!")
