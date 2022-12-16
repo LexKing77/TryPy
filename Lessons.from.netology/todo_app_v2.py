8e6befb9674aa049f9330959249354ce98e76b71
LexKing77 <lexking.aid@gmail.com>
2022-12-16 16:55:33 +0300

    changes todo_app_v2 and added functions_lk

--- Lessons.from.netology/todo_app_v2.py
+++ Lessons.from.netology/todo_app_v2.py
@@ -1,12 +1,22 @@
+import random
 
 HELP = """
 help - print a program guide.
 add - add a task to the list (we ask the user for the task name).
 show - print all of saved tasks.
 exit - stop the bot.
+random - add in the list a random task on today date.
 """
 
 tasks = {}
+random_tasks = ['Shake your ass', 'Fuck your mom', 'Lake your pussy']
+
+def add_todo(date, task):
+    if date in tasks:
+        tasks[date].append(task)
+    else:
+        tasks[date] = []
+        tasks[date].append(task)
 
 run = True
 
@@ -24,15 +34,15 @@ while run:
     elif command == "add":
        date = input('Please, enter the date of task: ')
        task = input('And now enter the name of task: ')
-       if date in tasks:
-           tasks[date].append(task)
-       else:
-           tasks[date] = []
-           tasks[date].append(task)
+       add_todo(date, task)
        print(f'The task "{task}" is added on "{date}" date.')
     elif command == "exit":
         run = False
         print("Bot is disabled. \nThanks for using me! Goodbye!")
+    elif command == "random":
+        add_todo('Today', random_tasks)
+        task = random.choice(random_tasks)
+        print(f'The "{task}" added on Today date')
     else:
         print("Unexpected command.")
         print("Bye!")
